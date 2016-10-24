07e707a45ddbea4239a3fa6c3cf809dd74d6ba75
Johannes Elias <joheli@gmx.net>
2016-10-24 18:24:42 +0200

    Update cleaner.r

--- cleaner.r
+++ cleaner.r
@@ -1,60 +1,6 @@
 # Version 5, 2016-09-01
 require(dplyr)
 
-# # .selectOneRow selektiert den ‰ltesten (oder neuesten) Datensatz
-# # gem‰ﬂ Bedingung "sel" ("sel" ist ein boolescher Vektor).
-# .selectOneRow <- function(tb, time.col, sel, decreasing=FALSE){ 
-# 	tb.sel <- tb[sel,]
-# 	return(tb.sel[order(tb.sel[,time.col], decreasing = decreasing),][1,])
-# }
-# 
-# # .cleanerOneCol putzt Doubletten raus
-# .cleanerOneCol <- function(tb, time.col, clean.col, decreasing=FALSE){
-# 	# wandle time.col in Date um, wenn notwendig
-# 	if (!inherits(tb[,time.col],"Date")) tb[,time.col] <- as.Date(tb[,time.col])
-# 	res <- data.frame()
-# 	for (u in unique(tb[,clean.col])) {
-# 		res <- rbind(res,.selectOneRow(tb,time.col,tb[,clean.col]==u,decreasing))
-# 	}
-# 	return(res)
-# }
-# 
-# # .cleanerOneCol putzt Doubletten raus
-# .cleanerOneCol1 <- function(tb, time.col, clean.col, decreasing=FALSE){
-#   # wandle time.col in Date um, wenn notwendig
-#   if (!inherits(tb[,time.col],"Date")) tb[,time.col] <- as.Date(tb[,time.col])
-#   res <- data.frame()
-#   u <- unique(tb[, clean.col])
-#   one <- function(x) .selectOneRow(tb, time.col, tb[,clean.col]==x, decreasing)
-#   res <- Reduce(rbind, Map(one, u))
-#   return(res)
-# }
-# 
-# # .mergedCol fasst mehrere Spalten einer data.frame in eine Spalte zusammen
-# # funktioniert analog zur Funktion 'paste'
-# .mergedCol  <- function(tb, cols = NULL, sep = "_") {
-# 	if (!is.null(cols)) tb <- tb[, cols]
-# 	if (!is.data.frame(tb)) stop("The selection must result in a data.frame!")
-# 	vc <- tb[,1]
-# 	if (ncol(tb) > 1) for (i in 2:ncol(tb)) vc <- paste(vc,tb[,i],sep=sep)
-# 	return(vc)
-# }
-# 
-# # .cleaner putzt ein data.frame, sodass nur die chronologisch ersten (decreasing=FALSE) 
-# # oder neuesten (decreasing=TRUE) Eintr‰ge ¸brigbleiben. Es sind beliebig viele 
-# # Kriterienspalten (clean.cols) mˆglich. 
-# .cleaner  <- function(tb, time.col, clean.cols, decreasing=FALSE, keepMergedCol=FALSE) {
-# 	extraCol <- ncol(tb) + 1
-# 	if (length(clean.cols) > 1) {
-# 		tb[, extraCol] <- .mergedCol(tb, clean.cols)
-# 	} else {
-# 		tb[, extraCol] <- tb[, clean.cols]
-# 	}
-# 	res <- .cleanerOneCol1(tb, time.col, extraCol, decreasing)
-# 	if (!keepMergedCol) res <- res[, -extraCol]
-# 	return(res)
-# }
-
 .cleaner  <- function(tb, time.col, clean.cols, decreasing = FALSE, 
                        keepExtraCols = FALSE) {
   tb$extraCol <- tb[, clean.cols[1]]
