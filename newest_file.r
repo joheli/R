b54d6f5484e0a6290a60273000ed0b11102b90ea
Johannes Elias <joheli@gmx.net>
2017-10-28 17:49:02 +0200

    add pattern

--- newest_file.r
+++ newest_file.r
@@ -1,7 +1,17 @@
+# Function returning the newest file in a directory
+#
+# Arguments
+#   path          directory (folder)
+#   pattern       pattern passed to dir
+#
+# Returns
+#   character specifying the path of the newest file or NA if none found
+#
+
 library(dplyr)
 
-newest.file <- function(path) {
-  fls <- file.info(dir(path, full.names = T))
+newest.file <- function(path, pattern = NULL) {
+  fls <- file.info(dir(path, full.names = T, pattern = pattern))
   fls$file <- rownames(fls)
   fls <- arrange(fls, desc(atime))
   return(fls$file[1])
