06198eee53cb0367d6e1923a8013bc3abf8c2034
Johannes Elias <joheli@gmx.net>
2017-11-10 17:25:41 +0100

    Add files via upload

--- factorer.r
+++ factorer.r
@@ -1,4 +1,4 @@
-# factorer, v 0.2
+# factorer, v 0.3
 # 
 # This function cleans up categorical variables and provides further options for aggregation of strata.
 # It uses a data.frame which contains search patterns and replace values in the first and second 
@@ -10,19 +10,33 @@
 #   fm        data.frame guiding the search and replace process
 #
 
-factorer <- function(f, fm) {
+factorer <- function(f, fm, default = c("same", "other"), other.text = "other") {
   # basic argument validation
   if (!is.data.frame(fm)) stop("fm needs to be a data.frame!")
   if (ncol(fm) < 2) stop("fm needs to have at least two columns!")
   if (is.factor(f)) f <- as.character(f)
   
+  # arg.match
+  default <- match.arg(default)
+  
   # copy f
   f.clean <- f
   
+  # replaced indices
+  ri <- numeric()
+  
   # for each pattern provide in the first column, replace entry with corresponding value in second column
   # i.e. search and replace
-  for (r in 1:nrow(fm)) f.clean[grep(fm[r, 1], f.clean)] <- as.character(fm[r, 2])
-
+  for (r in 1:nrow(fm)) {
+    i <- grep(fm[r, 1], f.clean)
+    f.clean[i] <- as.character(fm[r, 2])
+    ri <- c(ri, i)
+  }
+  
+  # label not replaced as other
+  ri <- unique(ri)
+  if (default == "other") f.clean[-ri] <- other.text
+  
   # if more than two columns are provided in the matrix, merge those values to the cleaned factor
   if (ncol(fm) > 2) {
     res1 <- data.frame(f.clean)
