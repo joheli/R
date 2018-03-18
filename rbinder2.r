f185c948e899cc82913d09693807b182abe00d45
Johannes Elias <joheli@gmx.net>
2018-03-18 17:27:36 +0100

    unique.field.name can now contain multiple entries
    unique.field.name can now be a character with more than one entry; e.g. if unique.field.name is c("field1", "field2"), a temporary key is generated from both fields to allow checking for new unique entries.

--- rbinder2.r
+++ rbinder2.r
@@ -1,6 +1,6 @@
 # rbinder2
 #
-# Version 0.1
+# Version 0.2
 #
 # Author: Johannes Elias (joheli@gmx.net)
 #
@@ -16,7 +16,7 @@
 #
 
 rbinder2 <- function(df.list,
-                    unique.field.name) {
+                     unique.field.name) {
   # stop if df.list is empty
   if (length(df.list) < 1) 
     stop(paste0(deparse(substitute(df.list)), " appears to be empty!"))
@@ -26,17 +26,31 @@ rbinder2 <- function(df.list,
   # stop if names of df.list contains NA
   if (any(is.na(names(df.list))))
     stop(paste0(deparse(substitute(df.list)), " has unnamed entries!"))
+  
+  # d is the dataframe returned at the end
   d <- data.frame()
+  
+  # nrows holds the number of rows contained within each assessed data.frame
   nrows <- numeric()
+  
+  # uniquerows lists the number of unique rows contributed by each assessed data.frame
   uniquerows  <- numeric()
+  
+  # function for temporary key generation
+  tk <- function(df, u) 
+    return(eval(parse(text = paste0("with(df, paste(", paste(u, collapse = ", "), "))"))))
+  
+  # loop through the list of data.frames 
   for (df.name in names(df.list)) {
-    df.current <- df.list[[df.name]]
-    nrows  <- c(nrows, nrow(df.current))
+    df.current <- df.list[[df.name]]      # current data.frame in loop
+    nrows  <- c(nrows, nrow(df.current))  # how much rows does the current data.frame have?
     if (nrow(d) == 0) {
+      # if d is empty (at start), set d to df.current
       d <- df.current
       uniquerows <- nrows
     } else {
-      df.current <- df.current[!(df.current[, unique.field.name] %in% d[, unique.field.name]), ]
+      # if d is not empty (after first iteration) reduce df.current to rows that are unique
+      df.current <- df.current[!(tk(df.current, unique.field.name) %in% tk(d, unique.field.name)), ]
       uniquerows <- c(uniquerows, nrow(df.current))
       d <- rbind(d, df.current)
     }
