d31882c7ab81c237198d7104ede29ab23e6d5128
Johannes Elias <joheli@gmx.net>
2019-05-20 10:46:40 +0200

    Surround conversion to factor with tryCatch

--- relabel_cat.r
+++ relabel_cat.r
@@ -2,14 +2,19 @@
 # zu "Rest"
 #
 # Parameter:
-# ctg			kategorische Variable (Faktor)
-# min.occ		Mindestvorkommen, unter welchem die Ausprägung
-#				zu Rest gezählt wird
+# ctg			    kategorische Variable (Faktor)
+# min.occ		  Mindestvorkommen, unter welchem die Ausprägung
+#				      zu Rest gezählt wird
 # rest.label	Bezeichnung des Restes
 
 relabel.cat <- function(ctg, min.occ=10, rest.label="Rest") {
-	ct <- ctg[,drop=T]
-	tb <- table(ct)
-	levels(ct)[levels(ct) %in% names(tb[tb < min.occ])] <- rest.label
-	return(relevel(ct, ref=rest.label))
+  tryCatch({
+    ctg <- as.factor(ctg)
+    ct <- ctg[, drop=T]
+  },
+  error = function(e) cat("Umwandlung in 'factor' gescheitert."))
+
+  tb <- table(ct)
+  levels(ct)[levels(ct) %in% names(tb[tb < min.occ])] <- rest.label
+  return(relevel(ct, ref=rest.label))
 }
